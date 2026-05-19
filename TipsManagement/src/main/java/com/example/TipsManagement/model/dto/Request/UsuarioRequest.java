e2d420c773557fd8821e5d97c43a0c39bad7193b
Darlei <darleifeix97@gmail.com>
2026-05-18 21:31:03 -0300

    Adicção de validações em UsuarioRequest, Notblank/email

--- TipsManagement/src/main/java/com/example/TipsManagement/model/dto/Request/UsuarioRequest.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/model/dto/Request/UsuarioRequest.java
@@ -1,14 +1,19 @@
 package com.example.TipsManagement.model.dto.Request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UsuarioRequest {
+
+    @NotBlank
     private String name;
 
     @Email
+    @NotBlank
     private String email;
 
+    @NotBlank
     private String password;
 }
