be1f0ae8f1532a81134884951dbadf3d03f3be90
Darlei <darleifeix97@gmail.com>
2026-05-18 21:38:39 -0300

    Criação de função no repository que busca bets com base no userId e status.

--- TipsManagement/src/main/java/com/example/TipsManagement/repository/IBetRepository.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/repository/IBetRepository.java
@@ -1,6 +1,7 @@
 package com.example.TipsManagement.repository;
 
 import com.example.TipsManagement.model.Bet;
+import com.example.TipsManagement.model.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ import java.util.Optional;
 public interface IBetRepository extends JpaRepository<Bet, Long> {
     Optional<Bet> findByIdAndUsuarioId(Long betId, Long userId);
     List<Bet> findAllByUsuarioId(Long userId);
+    List<Bet> findAllByUsuarioIdAndStatus(Long userId, Status status);
 
 }
