a5dc12e1a7f3766de842280c24d2c08a72b36f36
Darlei <darleifeix97@gmail.com>
2026-05-21 23:08:30 -0300

    Alterado Scheduler para chamar PandaScoreService ao invés do nome antigo MatchService

--- TipsManagement/src/main/java/com/example/TipsManagement/scheduler/GetMatchesScheduler.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/scheduler/GetMatchesScheduler.java
@@ -1,15 +1,15 @@
 package com.example.TipsManagement.scheduler;
 
-import com.example.TipsManagement.service.MatchService;
+import com.example.TipsManagement.service.PandaScoreService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GetMatchesScheduler {
-    private final MatchService matchService;
+    private final PandaScoreService pandaScoreService;
 
-    public GetMatchesScheduler(MatchService matchService) {
-        this.matchService = matchService;
+    public GetMatchesScheduler(PandaScoreService pandaScoreService) {
+        this.pandaScoreService = pandaScoreService;
     }
 
     @Scheduled(fixedDelay = 3600000)
@@ -17,6 +17,6 @@ public class GetMatchesScheduler {
 
         System.out.println("Sincronizando partidas...");
 
-        matchService.saveMatches();
+        pandaScoreService.savePandaScoreMatches();
     }
 }
