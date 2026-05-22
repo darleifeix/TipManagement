25baee8681fe317e9e8a1e851cc8eebd88b12644
Darlei <darleifeix97@gmail.com>
2026-05-21 22:19:16 -0300

    Adicionado comentário para habilitar Schedule na aplicação.

--- TipsManagement/src/main/java/com/example/TipsManagement/TipsManagementApplication.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/TipsManagementApplication.java
@@ -2,8 +2,10 @@ package com.example.TipsManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TipsManagementApplication {
 
 	public static void main(String[] args) {
