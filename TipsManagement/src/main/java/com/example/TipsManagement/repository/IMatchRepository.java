8d25dcc5553cdbc4056b2f5da22d3fc41dabc34d
Darlei <darleifeix97@gmail.com>
2026-06-01 23:06:24 -0300

    Criado em IMatchRepository.java duas funções, uma que busca e outra que deleta jogos antes da data recebida.

--- TipsManagement/src/main/java/com/example/TipsManagement/repository/IMatchRepository.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/repository/IMatchRepository.java
@@ -4,6 +4,12 @@ import com.example.TipsManagement.model.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
 @Repository
 public interface IMatchRepository extends JpaRepository<Match, Long> {
+    void deleteByMatchDateBefore(OffsetDateTime date);
+
+    List<Match> findByMatchDateBefore(OffsetDateTime date);
 }
